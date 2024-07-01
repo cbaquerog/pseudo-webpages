9e3ab56e538f6812f9fd31af19020556e28fbdb0
Carlos-Baquero <carlosbg14@gmail.com>
2024-07-01 11:26:58 +0200

    setting up flowbite

--- vue-webpage/tailwind.config.js
+++ vue-webpage/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
